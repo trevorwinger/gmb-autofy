861f324144d3ed4d7b67019feab8b8afd81a7eb5
Trevor Winger <winge159@d.umn.edu>
2020-08-10 21:46:44 -0500

    should be good to go for tyler

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ import './App.css';
 import Options from './Components/Options';
 
 function App() {
-  var options = ['Cherry Roofing is Vancouver, WA\'s top Roofing Contractor. Contact us today to get an estimate on your next roof replacement or roof repair!',
-  'Exteriors Plus is the leading residential roofing contractor in the Norton, OH area. Contact us today about your next roofing project!',
-  'Experienced, reliable roofers near you. Wyoming Roofing provides Sheridan & Gillette, WY with roof repair & replacement, backed by outstanding customer service.',
-  'Titan is your trusted Cincinnati Roofing Contractor. Whether you need roofing, siding, gutters or windows we can assist. Call for your free inspection',
-  'Allstate Exteriors is one of Ohio\'s leading Roofing Contractors. We specialize in everything from Residential to Commercial. Contact us today!'];
 
   var keywords = ['residential roofing', 'roofing contractor', 'residential roofing contractor', 'local roofing company', 
   'roofing', 'naples roofer', 'roof repair', 'roof replacement', 'flat roofing', 'shingle roofing', 'tile roofing'];
@@ -26,24 +21,55 @@ function App() {
     setBrandLocation(event.target.value);
   }
 
+  const introStyle = {margin: 50}
+
   return (
+
     <div className="App">
       <header className="App-header">
-        
+        <div style={introStyle}>
         <h1>GMB-Autofy</h1>
-        <div>
+        <img src={logo} className="App-logo" alt="logo" />
+        </div>
+
+        <div style={introStyle}>
+        <h2>Scope & Usage</h2>
+          <p>We needed a way to upload GMB content faster - here it is. 
+            <br></br>Input brand name & location 
+            <br></br>Use static keyord list & dynamic title and description
+            <br></br>Happy Hacking! :-)
+          </p>
+        </div>
+        
+        
+      </header>
+      <body className="App-body">
+
+      <div style={introStyle}>
           <h2>Brand</h2>
           <input value={brand} onChange={handleBrandChange}/>
+          <br></br>
+          <br></br>
         </div>
-        <div>
+
+        <div style={introStyle}>
           <h2>Location</h2>
           <input value={brandLocation} onChange={handleBrandLocationChange} />
+          <br></br>
+          <br></br>
         </div>
-        <h2>Keywords</h2>
-        <p>{keywords.join()}</p>
-        <Options loc={brandLocation} brand={brand} />
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+
+        <div style={introStyle}>
+          <h2>Keywords</h2>
+          <p>{keywords.join()}</p>
+          <br></br>
+          <br></br>
+        </div>
+
+        <div style={introStyle}>
+          <Options loc={brandLocation} brand={brand} />
+        </div>
+      </body>
     </div>
   );
 }
--- src/Components/Options.js
+++ src/Components/Options.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 
 const Options = (props) => {
+
   const s1 = props.brand + ' is ' + props.loc + '\'s top Roofing Contractor. Contact us today to get an estimate on your next roof replacement or roof repair!';
   const s2 = props.brand + ' is the leading residential roofing contractor in the ' + props.loc + ' area. Contact us today about your next roofing project!';
   const s3 = 'Experienced, reliable roofers near you. ' + props.brand + ' provides ' + props.loc + ' with roof repair & replacement, backed by outstanding customer service.';
   const s4 = props.brand + ' is your trusted '  + props.loc +' Roofing Contractor. Whether you need roofing, siding, gutters or windows we can assist. Call for your free inspection!';
   const s5 = props.brand + ' is one of ' + props.loc + '\'s leading Roofing Contractors. We specialize in everything from Residential to Commercial. Contact us today!';
+
   return (
       <div>
-          <h2>Title & Description Options</h2>
+          <h2>Title & Description Option(s)</h2>
           <ul>
             <li>{s1}</li>
             <li>{s2}</li>
