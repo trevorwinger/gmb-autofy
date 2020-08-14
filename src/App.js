82e7d84cf2c150d3ec07ddc0005578019bd1f353
Trevor Winger <winge159@d.umn.edu>
2020-08-13 23:36:22 -0500

    made ui optimizations & code clean up

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ import Keywords from './Components/Keywords';
 
 function App() {
 
-  var keywords = ['residential roofing', 'roofing contractor', 'residential roofing contractor', 'local roofing company', 
-  'roofing', 'naples roofer', 'roof repair', 'roof replacement', 'flat roofing', 'shingle roofing', 'tile roofing'];
-
   const [brand, setBrand] = useState('');
   const [brandLocation, setBrandLocation] = useState('');
 
@@ -22,49 +19,39 @@ function App() {
     setBrandLocation(event.target.value);
   }
 
-  const introStyle = {margin: 5}
-
   return (
 
     <div className="App">
       <header className="App-header">
-        <div style={introStyle}>
-        <h1>GMB-Autofy</h1>
-        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <h1>gmb-autofy</h1>
+          <img src={logo} className="App-logo" alt="logo" />
         </div>
 
-        <div style={introStyle}>
-        <h2>Scope & Usage</h2>
-          <p>We needed a way to upload GMB content faster - here it is. 
-            <br></br>Input brand name & location 
-            <br></br>Use static keyord list & dynamic title and description
-            <br></br>Happy Hacking! :-)
-          </p>
-        </div>
-        
-        
+        <body>
+
+          <div className="flex-container">
+
+            <div className="flex-child">
+              <h2>Brand</h2>
+              <input value={brand} onChange={handleBrandChange}/>
+            </div>
+
+            <div className="flex-child">
+            <h2>Location</h2>
+            <input value={brandLocation} onChange={handleBrandLocationChange} />
+          </div>
+          </div>
+
+          <div>
+            <br></br><br></br>
+            <Keywords loc={brandLocation} />
+            <br></br>
+            <Options loc={brandLocation} brand={brand} />
+          </div>
+        </body>
       </header>
-      <body className="App-body">
-
-      <div style={introStyle}>
-          <h2>Brand</h2>
-          <input value={brand} onChange={handleBrandChange}/>
-          <br></br>
-          <br></br>
-        </div>
-
-        <div style={introStyle}>
-          <h2>Location</h2>
-          <input value={brandLocation} onChange={handleBrandLocationChange} />
-          <br></br>
-          <br></br>
-        </div>
-
-        <div style={introStyle}>
-          <Keywords loc={brandLocation} />
-          <Options loc={brandLocation} brand={brand} />
-        </div>
-      </body>
+      
     </div>
   );
 }
--- src/Components/Options.js
+++ src/Components/Options.js
@@ -3,21 +3,17 @@ import React from 'react';
 
 const Options = (props) => {
 
-  const s1 = props.brand + ' is ' + props.loc + '\'s top Roofing Contractor. Contact us today to get an estimate on your next roof replacement or roof repair!';
-  const s2 = props.brand + ' is the leading residential roofing contractor in the ' + props.loc + ' area. Contact us today about your next roofing project!';
-  const s3 = 'Experienced, reliable roofers near you. ' + props.brand + ' provides ' + props.loc + ' with roof repair & replacement, backed by outstanding customer service.';
-  const s4 = props.brand + ' is your trusted '  + props.loc +' Roofing Contractor. Whether you need roofing, siding, gutters or windows we can assist. Call for your free inspection!';
-  const s5 = props.brand + ' is one of ' + props.loc + '\'s leading Roofing Contractors. We specialize in everything from Residential to Commercial. Contact us today!';
+  const sentences = [props.brand + ' is ' + props.loc + '\'s top Roofing Contractor. Contact us today to get an estimate on your next roof replacement or roof repair!',
+  props.brand + ' is the leading residential roofing contractor in the ' + props.loc + ' area. Contact us today about your next roofing project!',
+  'Experienced, reliable roofers near you. ' + props.brand + ' provides ' + props.loc + ' with roof repair & replacement, backed by outstanding customer service.',
+  props.brand + ' is your trusted '  + props.loc +' Roofing Contractor. Whether you need roofing, siding, gutters or windows we can assist. Call for your free inspection!',
+  props.brand + ' is one of ' + props.loc + '\'s leading Roofing Contractors. We specialize in everything from Residential to Commercial. Contact us today!']
 
   return (
       <div>
           <h2>Title & Description Option(s)</h2>
           <ul>
-            <li>{s1}</li>
-            <li>{s2}</li>
-            <li>{s3}</li>
-            <li>{s4}</li>
-            <li>{s5}</li>
+              {sentences.map((s) => <li>{s}</li>)}
           </ul>
       </div>
   )
